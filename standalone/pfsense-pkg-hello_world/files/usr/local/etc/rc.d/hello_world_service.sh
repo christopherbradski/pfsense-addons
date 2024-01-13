eeff65700774aebc56ef10a441dbad68b583b970
Chris Bradski <chris.bradski@giant.ai>
2024-01-13 01:22:42 -0800

    add hello world

---
+++ standalone/pfsense-pkg-hello_world/files/usr/local/etc/rc.d/hello_world_service.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+while true; do
+    echo "Hello"
+    sleep 60
+done
---
+++ standalone/pfsense-pkg-hello_world/work/stage/usr/local/www/hello_world.php
@@ -0,0 +1,9 @@
+<?php
+require_once("guiconfig.inc");
+$pgtitle = array("Services", "Hello World");
+include("head.inc");
+
+echo "Hello World";
+
+include("foot.inc");
+?>
