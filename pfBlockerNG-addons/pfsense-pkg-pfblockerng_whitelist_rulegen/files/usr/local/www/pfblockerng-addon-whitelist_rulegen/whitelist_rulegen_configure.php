0d6159831058816b5c635b071c90f07c299919c8
Chris Bradski <chris.bradski@giant.ai>
2024-01-16 22:36:01 -0800

    update whitelist_rulegen_configure.php to pfsense theme

--- pfBlockerNG-addons/pfsense-pkg-pfblockerng_whitelist_rulegen/files/usr/local/www/pfblockerng-addon-whitelist_rulegen/whitelist_rulegen_configure.php
+++ pfBlockerNG-addons/pfsense-pkg-pfblockerng_whitelist_rulegen/files/usr/local/www/pfblockerng-addon-whitelist_rulegen/whitelist_rulegen_configure.php
@@ -1,7 +1,6 @@
 <?php
 require_once("guiconfig.inc");
-
-include("head.inc");
+require_once("head.inc");
 
 if ($_POST) {
     if (isset($_POST['username'])) {
@@ -15,28 +14,40 @@ if ($_POST) {
         $config['pfblockerng-addon-whitelist_rulegen']['apikey'] = $_POST['apikey'];
         write_config("Saved API Key for pfblockerng-addon-whitelist_rulegen");
     }
-
 }
 
 $username = $config['pfblockerng-addon-whitelist_rulegen']['username'];
 $apikey = $config['pfblockerng-addon-whitelist_rulegen']['apikey'];
+
+$pgtitle = array("Whitelist Rulegen Configuration");
+include("head.inc");
 ?>
 
-<!DOCTYPE html>
-<html>
-<body>
-    <form action="whitelist_rulegen_configure.php" method="post">
-        Username: <input type="text" name="username" value="<?= htmlspecialchars($username) ?>">
-        <input type="submit" value="Save Username">
-    </form>
-
-    <form action="whitelist_rulegen_configure.php" method="post">
-        API Key: <input type="password" name="apikey" value="<?= htmlspecialchars($apikey) ?>">
-        <input type="submit" value="Save API Key">
-    </form>
-
-
-</body>
-</html>
+<div class="panel panel-default">
+    <div class="panel-heading">
+        <h2 class="panel-title">pfSense API Credentials</h2>
+    </div>
+    <div class="panel-body">
+        <form action="whitelist_rulegen_configure.php" method="post" class="form-horizontal">
+            <div class="form-group">
+                <label for="username" class="col-sm-2 control-label">Username</label>
+                <div class="col-sm-10">
+                    <input type="text" name="username" class="form-control" id="username" value="<?= htmlspecialchars($username) ?>">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="apikey" class="col-sm-2 control-label">API Key</label>
+                <div class="col-sm-10">
+                    <input type="password" name="apikey" class="form-control" id="apikey" value="<?= htmlspecialchars($apikey) ?>">
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-sm-10 col-sm-offset-2">
+                    <button type="submit" class="btn btn-primary">Save</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
 
 <?php include("foot.inc"); ?>
